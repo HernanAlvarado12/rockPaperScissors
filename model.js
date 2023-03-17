95c3ef0b57649e0299c9a82cb5315e10e208ec54
halvaradop_ <hernanvid123@gmail.com>
2023-03-16 22:34:27 -0500

    update method of cleanNodes for receive names parameters

--- model.js
+++ model.js
@@ -1,6 +1,5 @@
 const options = [...document.getElementById('sectionOptions').children]
 const lines = [...document.querySelectorAll('#sectionOptions > article > div + span.absolute')]
-console.log(lines)
 
 document.addEventListener('click', event => {
     /**
@@ -8,11 +7,11 @@ document.addEventListener('click', event => {
      */
     const target = event.target
     if(target.matches('#sectionOptions :is(#optionPaper, #optionPaper :is(div), #optionPaper :is(figure, img))')) {
-        cleanNodes(options, '#optionPaper, #youPicked', lines)
+        cleanNodes({ optionsNodes: options, nodeSelector: '#optionPaper, #youPicked', lines })
     }else if(target.matches('#sectionOptions :is(#optionScissors, #optionScissors :is(div), #optionScissors :is(figure, img))')) {
-        cleanNodes(options, '#optionScissors, #youPicked', lines)
+        cleanNodes({ optionsNodes: options, nodeSelector: '#optionScissors, #youPicked', lines })
     }else if(target.matches('#sectionOptions :is(#optionRock, #optionRock :is(div), #optionRock :is(figure, img))')) {
-        cleanNodes(options, '#optionRock, #youPicked', lines)
+        cleanNodes({ optionsNodes: options, nodeSelector: '#optionRock, #youPicked', lines })
     }
 })
 
@@ -22,7 +21,7 @@ document.addEventListener('click', event => {
  * @param {Array<Element>} optionsNodes 
  * @param {String} nodeSelector 
  */
-function cleanNodes(optionsNodes, nodeSelector, lines) {
+function cleanNodes({ optionsNodes, nodeSelector, lines }) {
     optionsNodes.filter(optionNode => !optionNode.matches(nodeSelector))
                 .forEach(optionNode => optionNode.classList.add('hidden'))
     cleanLines(lines)
